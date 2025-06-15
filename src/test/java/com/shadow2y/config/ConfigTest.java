ae333930793aad69f4a041c1a83aa29250bdee86
shadow2y <90892814+Shadow2Y@users.noreply.github.com>
2025-06-15 21:25:24 +0530

    added more tests

--- src/test/java/com/shadow2y/config/ConfigTest.java
+++ src/test/java/com/shadow2y/config/ConfigTest.java
@@ -1,6 +1,7 @@
 package com.shadow2y.config;
 
 import com.shadow2y.config.core.Config;
+import com.shadow2y.config.core.ConfigException;
 import com.shadow2y.config.core.ConfigFactory;
 
 import ch.qos.logback.classic.Logger;
@@ -12,24 +13,56 @@ public class ConfigTest {
 
     private static final Logger log = (Logger) LoggerFactory.getLogger(ConfigTest.class);
 
-    private final Config config;
 
-    public ConfigTest(String filepath) {
-        config = ConfigFactory.createConfig(filepath);
+    public static void positiveConfig(String filepath) {
+        Config config = ConfigFactory.createConfig(filepath);
+        log.info(":-: Running - POSITIVE - Tests :-:");
+        String value = config.getString(testKey);
+        log.info("Read Value from - POSITIVE - file :: {}",value);
+
+        if(!value.equals(testValue)) {
+            throw new RuntimeException("Test failed :: Expected :: "+testValue+" :: found :: "+value);
+        }
+        log.info("\n>>>>>> :-: Test :: positiveConfig :: Passed Successfully OoO :-:\n");
     }
 
-    public void testConfig() {
-        log.info(":-: Running Tests :-:");
+    public static void negativeConfig(String filepath) {
+        Config config = ConfigFactory.createConfig(filepath);
+        log.info(":-: Running - NEGATIVE - Tests :-:");
         String value = config.getString(testKey);
-        log.info("Read Value from file :: {}",value);
-        assert value.equals(testValue);
+        log.info("Read Value from - NEGATIVE - file :: {}",value);
+
+        if(value.equals(testValue)) {
+            throw new RuntimeException("Test failed :: Expected not :: "+testValue+" :: found :: "+value);
+        }
+        log.info("\n>>>>>> :-: Test :: negativeConfig :: Passed Successfully OoO :-:\n");
+    }
+
+    public static void nonExistentConfig(String filepath) {
+        Config config = ConfigFactory.createConfig(filepath);
+        log.info(":-: Running - Non-Existent - Tests :-:");
+        try {
+            String value = config.getString(testKey);
+            log.info("Read Value from - Non-Existent - file :: {}", value);
+        } catch (ConfigException e) {
+            log.info("\n>>>>>> :-: Test :: nonExistentConfig :: Passed Successfully OoO :-:\n");
+            return;
+        }
+        throw new RuntimeException("Test failed :: Didn't Expect Key :: "+testValue);
     }
 
     public static void main(String[] args) {
         log.info(":-: Instantiating ConfigFactory :-:");
-        String filepath = System.getProperty("user.dir") + "\\" + "config.properties";
-        ConfigTest configTest = new ConfigTest(filepath);
-        configTest.testConfig();
+        String[] positiveTestFiles = {"config.properties","config.json","config.yaml","config.yml"};
+
+        String baseFilepath = System.getProperty("user.dir") + "\\";
+        positiveConfig(baseFilepath + positiveTestFiles[0]);
+        positiveConfig(baseFilepath + positiveTestFiles[1]);
+        positiveConfig(baseFilepath + positiveTestFiles[2]);
+        positiveConfig(baseFilepath + positiveTestFiles[3]);
+
+        negativeConfig(baseFilepath + "negative.properties");
+        nonExistentConfig(baseFilepath + "nonexistent.properties");
     }
 
 }
